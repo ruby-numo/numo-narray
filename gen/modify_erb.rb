7fbc76e21e776967d33553313c3a0024bc0ee6b6
Masahiro TANAKA <masa16.tanaka@gmail.com>
2016-04-03 23:25:06 +0900

    reduce #line insertion

--- gen/modify_erb.rb
+++ gen/modify_erb.rb
@@ -19,24 +19,25 @@ class ERB
     src_with_cpp_line
   end
 
-  RE_TRIM = /^; _erbout.concat "\s+";\s*/
-  RE_BREAK = /^; _erbout.concat "/ #"
-
   def src_with_cpp_line
     linebreak = false
     @src.each_line.with_index.map do |line, num|
-      trim = line.sub(RE_TRIM,"")
-      if num==1
+      if num==0
+        # skip
+      elsif num==1
         s = "#line #{num} \"#{@filename}\"\n".dump
         line.sub!(/_erbout = '';/, "_erbout = #{s};")
-      elsif RE_BREAK =~ trim
-        if linebreak
-          s = "#line #{num} \"#{@filename}\"\n".dump
-          line = ";_erbout.concat(#{s})"+line
-          linebreak = false
-        end
-      elsif num > 1
+      elsif /^; _erbout\.concat "{\\n"$/ =~ line
+        # avoid following case:
+        #   function()
+        #   #line 9
+        #   {
+      elsif /^; _erbout\.concat/ !~ line
         linebreak = true
+      elsif /^; _erbout\.concat "/ =~ line && linebreak
+        s = "#line #{num} \"#{@filename}\"\n".dump
+        line = ";_erbout.concat(#{s})"+line
+        linebreak = false
       end
       line
     end.join
