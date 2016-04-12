15b2ef861e4e467ccd010d99e16cb30d2583804b
Masahiro TANAKA <masa16.tanaka@gmail.com>
2016-04-12 23:06:36 +0900

    smart line counter using CountLnString

--- ext/numo/narray/gen/line_number.rb
+++ ext/numo/narray/gen/line_number.rb
@@ -1,3 +1,48 @@
+class CountLnString < String
+
+  def initialize(filename)
+    @filename = filename
+    @lnchar = "\n"
+    @buf = ""
+    @str = ""
+    @countln = 1
+    @current = 1
+    super(report_line)
+  end
+
+  def report_line
+    "#line #{@countln} \"#{@filename}\"\n"
+  end
+
+  def concat(n,s)
+    case n
+    when 0
+      @buf.concat(s)
+      @str.concat(s)
+    when 1
+      @buf.concat(s)
+    else
+      super(s)
+    end
+  end
+
+  def ln(n)
+    if @current != @countln || @postpone
+      if /^\s*$/ =~ @str || /\A#line / =~ @buf
+        @postpone = true
+      else
+        concat(2,report_line)
+        @postpone = false
+      end
+    end
+    concat(2,@buf)
+    @countln = @current + @buf.count(@lnchar)
+    @current = n
+    @buf = ""
+    @str = ""
+  end
+end
+
 class ERB
   alias result_orig result
 
@@ -9,6 +54,7 @@ class ERB
         eval(src, b, (@filename || '(erb)'), 0)
       }.call
     else
+      #open("tmpout","w"){|f| f.write src} if /dtype/=~@filename
       eval(src, b, (@filename || '(erb)'), 0)
     end
   end
@@ -20,24 +66,16 @@ class ERB
   end
 
   def src_with_cpp_line
-    linebreak = false
     @src.each_line.with_index.map do |line, num|
+      line.gsub!(/_erbout.concat "/,'_erbout.concat 0,"')
+      line.gsub!(/_erbout.concat\(/,'_erbout.concat(1,')
       if num==0
         # skip
       elsif num==1
-        s = "#line #{num} \"#{@filename}\"\n".dump
-        line.sub!(/_erbout = '';/, "_erbout = #{s};")
-      elsif /^; _erbout\.concat "{\\n"$/ =~ line
-        # avoid following case:
-        #   function()
-        #   #line 9
-        #   {
-      elsif /^; _erbout\.concat/ !~ line
-        linebreak = true
-      elsif /^; _erbout\.concat "/ =~ line && linebreak
-        s = "#line #{num} \"#{@filename}\"\n".dump
-        line = ";_erbout.concat(#{s})"+line
-        linebreak = false
+        f = @filename.dump
+        line.sub!(/_erbout = '';/, "_erbout = CountLnString.new(#{f});")
+      elsif /^; _erbout\./ =~ line
+        line.sub!(/^;/,";_erbout.ln(#{num});")
       end
       line
     end.join
