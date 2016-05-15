0543480460e39710102a4916885c0e4eadfae7b0
Masahiro TANAKA <masa16.tanaka@gmail.com>
2016-05-15 22:14:25 +0900

    update median.c

--- ext/numo/narray/gen/tmpl/median.c
+++ ext/numo/narray/gen/tmpl/median.c
@@ -1,32 +1,23 @@
 static void
 <%=c_iter%>(na_loop_t *const lp)
 {
-    size_t i, n;
+    size_t n;
     char *p1, *p2;
-    ssize_t s1;
-    size_t *idx1;
     dtype *buf;
 
-    //INIT_COUNTER(lp, n);
-    n = lp->args[0].shape[0];
-    INIT_PTR_IDX(lp, 0, p1, s1, idx1);
+    INIT_COUNTER(lp, n);
+    p1 = (lp->args[0]).ptr + (lp->args[0].iter[0]).pos;
     p2 = (lp->args[1]).ptr + (lp->args[1].iter[0]).pos;
-    buf = (dtype*)(lp->opt_ptr);
-    if (idx1) {
-        for (i=0; i<n; i++) {
-            buf[i] = *(dtype*)(p1+idx1[i]);
-        }
-    } else {
-        for (i=0; i<n; i++) {
-            buf[i] = *(dtype*)(p1+s1*i);
-        }
-    }
+    buf = (dtype*)p1;
+
     <%=tp%>_qsort(buf, n, sizeof(dtype));
+
     <% if is_float %>
     for (; n; n--) {
         if (!isnan(buf[n-1])) break;
     }
     <% end %>
+
     if (n==0) {
         *(dtype*)p2 = buf[0];
     }
@@ -41,7 +32,13 @@ static void
 static VALUE
 <%=c_func%>(int argc, VALUE *argv, VALUE self)
 {
-    VALUE v;
-    v = na_median_main(argc, argv, self, <%=c_iter%>);
-    return numo_<%=tp%>_extract(v);
+    VALUE reduce;
+    ndfunc_arg_in_t ain[2] = {{cT,0},{sym_reduce,0}};
+    ndfunc_arg_out_t aout[1] = {{INT2FIX(0),0}};
+    ndfunc_t ndf = {<%=c_iter%>, NDF_FLAT_REDUCE, 2,1, ain,aout};
+
+    self = na_copy(self);
+    reduce = na_reduce_dimension(argc, argv, 1, &self); // v[0] = self
+
+    return na_ndloop(&ndf, 2, self, reduce);
 }
