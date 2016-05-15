c74bc4141d2f5083d15522a1522edc21c5c5c229
Masahiro TANAKA <masa16.tanaka@gmail.com>
2016-05-15 17:55:08 +0900

    apply new NDF_FLAT_REDUCE on accum template

--- ext/numo/narray/gen/tmpl/accum.c
+++ ext/numo/narray/gen/tmpl/accum.c
@@ -3,46 +3,22 @@ static void
 {
     size_t   i;
     char    *p1, *p2;
-    ssize_t  s1, s2;
-    size_t  *idx1;
+    ssize_t  s1;
     dtype    x, y;
 
     INIT_COUNTER(lp, i);
-    INIT_PTR_IDX(lp, 0, p1, s1, idx1);
-    INIT_PTR(lp, 1, p2, s2);
+    INIT_PTR(lp, 0, p1, s1);
+    p2 = lp->args[1].ptr + lp->args[1].iter[0].pos;
+    //INIT_PTR(lp, 1, p2, s2);
     //printf("n=%ld,s1=%ld,s2=%ld ",i,s1,s2);
-    if (s2==0) {
-        // Reduce loop
-        GET_DATA(p2,dtype,y);
-        if (idx1) {
-            for (; i--;) {
-                GET_DATA_INDEX(p1,idx1,dtype,x);
-                m_<%=method%>(x,y);
-            }
-        } else {
-            for (; i--;) {
-                GET_DATA_STRIDE(p1,s1,dtype,x);
-                m_<%=method%>(x,y);
-            }
-        }
-        SET_DATA(p2,dtype,y);
-    } else {
-        if (idx1) {
-            for (; i--;) {
-                GET_DATA_INDEX(p1,idx1,dtype,x);
-                GET_DATA(p2,dtype,y);
-                m_<%=method%>(x,y);
-                SET_DATA_STRIDE(p2,s2,dtype,y);
-            }
-        } else {
-            for (; i--;) {
-                GET_DATA_STRIDE(p1,s1,dtype,x);
-                GET_DATA(p2,dtype,y);
-                m_<%=method%>(x,y);
-                SET_DATA_STRIDE(p2,s2,dtype,y);
-            }
-        }
+    // Reduce loop
+    GET_DATA_STRIDE(p1,s1,dtype,y);
+    i--;
+    for (; i--;) {
+        GET_DATA_STRIDE(p1,s1,dtype,x);
+        m_<%=method%>(x,y);
     }
+    SET_DATA(p2,dtype,y);
 }
 
 /*
@@ -55,11 +31,11 @@ static VALUE
 <%=c_func%>(int argc, VALUE *argv, VALUE self)
 {
     VALUE v, reduce;
-    ndfunc_arg_in_t ain[3] = {{cT,0},{sym_reduce,0},{sym_init,0}};
+    ndfunc_arg_in_t ain[2] = {{cT,0},{sym_reduce,0}};
     ndfunc_arg_out_t aout[1] = {{cT,0}};
-    ndfunc_t ndf = { <%=c_iter%>, STRIDE_LOOP_NIP|NDF_FLAT_REDUCE, 3, 1, ain, aout };
+    ndfunc_t ndf = { <%=c_iter%>, STRIDE_LOOP_NIP|NDF_FLAT_REDUCE, 2, 1, ain, aout };
 
     reduce = na_reduce_dimension(argc, argv, 1, &self);
-    v =  na_ndloop(&ndf, 3, self, reduce, m_<%=method%>_init);
+    v =  na_ndloop(&ndf, 2, self, reduce);
     return numo_<%=tp%>_extract(v);
 }
