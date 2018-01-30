b6066b2fc9c08961569ab3bb29fc48f1db25b6ee
Masahiro TANAKA <masa16.tanaka@gmail.com>
2018-01-30 17:30:36 +0900

    v0.9.1.1

--- ext/numo/narray/numo/narray.h
+++ ext/numo/narray/numo/narray.h
@@ -13,8 +13,8 @@ extern "C" {
 #endif
 #endif
 
-#define NARRAY_VERSION "0.9.1.0"
-#define NARRAY_VERSION_CODE 910
+#define NARRAY_VERSION "0.9.1.1"
+#define NARRAY_VERSION_CODE 911
 
 #include <math.h>
 #include "numo/compat.h"
