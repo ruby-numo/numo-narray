2f23f79a13e5d34009302356cac7d5f194f2da43
Masahiro TANAKA <masa16.tanaka@gmail.com>
2020-05-06 10:35:17 +0900

    fix NArray#store: drop automatic 'to_a' conversion (#162)

--- ext/numo/narray/gen/tmpl/store.c
+++ ext/numo/narray/gen/tmpl/store.c
@@ -30,15 +30,6 @@ static VALUE
         }
     }
 
-    if (rb_respond_to(obj,id_to_a)) {
-        obj = rb_funcall(obj,id_to_a,0);
-        if (TYPE(obj)!=T_ARRAY) {
-            rb_raise(rb_eTypeError, "`to_a' did not return Array");
-        }
-        <%=store_array.c_func%>(self,obj);
-        return self;
-    }
-
     <% if is_object %>
     robject_store_numeric(self,obj);
     <% else %>
